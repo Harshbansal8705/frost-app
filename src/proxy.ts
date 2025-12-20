2e4ee9d1e7b9e8d0545b8a775b86116a90b61456
Harshbansal8705 <harsh@harshbansal.in>
2025-12-20 17:40:12 +0530

    feat: Redirect authenticated users from sign-in to dashboard.

--- src/proxy.ts
+++ src/proxy.ts
@@ -11,9 +11,12 @@ export async function proxy(request: NextRequest) {
   if (!session) {
     return NextResponse.redirect(new URL('/auth/signin', request.url))
   }
+  if (request.nextUrl.pathname === "/auth/signin") {
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: '/dashboard/:path*',
+  matcher: ['/dashboard/:path*', "/auth/:path*"],
 }
