37eb50a00cae87fbc254224564740f99fdc7365c
Harshbansal8705 <harsh@harshbansal.in>
2025-12-21 04:45:25 +0530

    style: Adjust toaster position from top-right to top-center.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default async function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider session={session}>{children}</AuthProvider>
-        <Toaster richColors closeButton position="top-right" />
+        <Toaster richColors closeButton position="top-center" />
       </body>
     </html>
   );
